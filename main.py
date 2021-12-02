aa7fb8bfe0a74f0bfe3920ea8cfe29887434d26d
NotSwan <31709744+NotSwan@users.noreply.github.com>
2021-12-02 12:32:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,88 +1,83 @@
 import pygame, sys
-import pygame.freetype
+import pygame.freetype 
 from pygame.locals import *
-#import puck, ball
-
-pygame.init()
-fps = pygame.time.Clock()
- 
+from ball import ball
 # Define some colors
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
- 
-# Open a new window
-size = (700, 700)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Pong")
-menuFont = pygame.freetype.SysFont("Copperplate", 40)
-
-selected = True
-cursor = 0
-gameMode = -99
-textSpacing = 30
-opt1, opt1Bound = menuFont.render("AI", fgcolor=WHITE)
-opt2, opt2Bound = menuFont.render("Local", fgcolor=WHITE)
-opt3, opt3Bound = menuFont.render("Online", fgcolor=WHITE)
-while selected:
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-              pygame.quit()
-              sys.exit()
+class Pong:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640, 400
+        self.cursor = 0
+        self.gameMode = -99
+        self.textSpacing = 30
+        self.selected = True
+    def on_init(self):
+        pygame.init()
+        pygame.display.set_caption("Pong")
+        self.fps = pygame.time.Clock()
+        self.menuFont = pygame.freetype.SysFont("Copperplate", 40)
+        self.screen = pygame.display.set_mode(self.size)
+        self._running = True
+        self.opt1, self.opt1Bound = self.menuFont.render("AI", fgcolor=WHITE)
+        self.opt2, self.opt2Bound = self.menuFont.render("Local", fgcolor=WHITE)
+        self.opt3, self.opt3Bound = self.menuFont.render("Online", fgcolor=WHITE)
+        self.startx = (self.screen.get_width() - (self.opt1.get_width() + self.opt2.get_width() + self.opt3.get_width() + 30)) / 2
+        self.starty = self.opt1.get_height() / 2 + self.screen.get_height() / 2
+        self.ballObj = ball()
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
         if event.type == KEYUP:
-            if cursor == 1:
-                cursor = -1
+            if self.cursor == 1:
+                self.cursor = -1
             else:
-                cursor += 1
+                self.cursor += 1
         if event.type == KEYDOWN:
-            if cursor == -1:
-                cursor = 1
+            if self.cursor == -1:
+                self.cursor = 1
             else:
-                cursor -= 1
+                self.cursor -= 1
         if event.type == K_RETURN:
-            selected = False
-            gameMode = cursor
+            self.selected = False
+            self.gameMode = self.cursor
+    def on_loop(self):
+        pass
+    def on_render(self):
+        if (self.selected):
+            self.screen.fill(BLACK)
+            self.ballObj.draw(self.screen, (self.screen.get_width(),self.screen.get_height()))
+            self.screen.blit(self.opt1, (self.startx, self.starty))
+            self.screen.blit(self.opt2, (self.startx + self.opt1.get_width() + self.textSpacing, self.starty))
+            self.screen.blit(self.opt3, (self.startx + self.opt1.get_width() + self.opt2.get_width() + self.textSpacing * 2, self.starty))
+            if self.cursor == -1:
+                pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx, self.starty, self.opt1.get_width(), self.opt1.get_height()),  2)
+            if self.cursor == 0:
+                pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx + self.opt1.get_width() + self.textSpacing, self.starty, self.opt2.get_width(), self.opt2.get_height()),  2)
+            if self.cursor == 1:
+                pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx + self.opt1.get_width() + self.opt2.get_width() + self.textSpacing*2, self.starty, self.opt3.get_width(), self.opt3.get_height()),  2)
 
-    
-
-    screen.fill(BLACK)
-    startx = (screen.get_width() - (opt1.get_width() + opt2.get_width() + opt3.get_width() + 30)) / 2
-    starty = opt1.get_height() / 2 + screen.get_height() / 2
-    screen.blit(opt1, (startx, starty))
-    screen.blit(opt2, (startx + opt1.get_width() + textSpacing, starty))
-    screen.blit(opt3, (startx + opt1.get_width() + opt2.get_width() + textSpacing * 2, starty))
-
-    if cursor == -1:
-        pygame.draw.rect(screen, RED, pygame.Rect(startx, starty, opt1.get_width(), opt1.get_height()),  2)
-    if cursor == 0:
-        pygame.draw.rect(screen, RED, pygame.Rect(startx + opt1.get_width() + textSpacing, starty, opt2.get_width(), opt2.get_height()),  2)
-    if cursor == 1:
-        pygame.draw.rect(screen, RED, pygame.Rect(startx + opt1.get_width() + opt2.get_width() + textSpacing*2, starty, opt3.get_width(), opt3.get_height()),  2)
-
-    pygame.display.update()
-    fps.tick(60)
+        pygame.display.update()
+        self.fps.tick(60)
+    def on_cleanup(self):
+        pygame.quit()
+        sys.exit()
  
-while True:
-    #Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-              pygame.quit()
-              sys.exit()
-        if event.type == KEYUP:
-            pass
-        if event.type == KEYUP:
-            pass
-        if event.type == K_KP_ENTER:
-            pass
-    #input logic should go here
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
  
-    #clear the screen to black. 
-    screen.fill(BLACK)
-    #Draw the net
-    pygame.draw.line(screen, WHITE, [349, 0], [349, 500], 5)
- 
-    #update the screen with what we've drawn.
-    pygame.display.update()
-     
-    fps.tick(60) #delay this loop to 60 times a second
- 
+        while(self._running ):
+            for event in pygame.event.get(): # User did something
+                self.on_event(event)
+                self.on_render()
+            self.on_render()
+            self.on_loop()
+        self.on_cleanup()
+        
+if __name__ == "__main__" :
+    playPong = Pong()
+    playPong.on_execute()
