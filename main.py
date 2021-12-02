d7ef22648a1e352cc51d4fcbbe3791d81b7fba67
NotSwan <31709744+NotSwan@users.noreply.github.com>
2021-12-02 13:58:45 -0500

    Menu bug fix
    Fixed navigation by handling event.key instead of event.type
    
    Created render_menu function removing code from general render function

--- main.py
+++ main.py
@@ -31,35 +31,29 @@ class Pong:
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
-        if event.type == KEYUP:
-            if self.cursor == 1:
-                self.cursor = -1
-            else:
-                self.cursor += 1
         if event.type == KEYDOWN:
-            if self.cursor == -1:
-                self.cursor = 1
-            else:
-                self.cursor -= 1
-        if event.type == K_RETURN:
-            self.selected = False
-            self.gameMode = self.cursor
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
+                if self.cursor == -1:
+                    self.cursor = 1
+                elif self.cursor == 1:
+                    self.cursor = 0
+                else:
+                    self.cursor = -1
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                if self.cursor == -1:
+                    self.cursor = 0
+                elif self.cursor == 1:
+                    self.cursor = -1
+                else:
+                    self.cursor = 1
+            if event.type == K_RETURN:
+                self.selected = False
+                self.gameMode = self.cursor
     def on_loop(self):
         pass
     def on_render(self):
         if (self.selected):
-            self.screen.fill(BLACK)
-            self.ballObj.draw(self.screen, (self.screen.get_width(),self.screen.get_height()))
-            self.screen.blit(self.opt1, (self.startx, self.starty))
-            self.screen.blit(self.opt2, (self.startx + self.opt1.get_width() + self.textSpacing, self.starty))
-            self.screen.blit(self.opt3, (self.startx + self.opt1.get_width() + self.opt2.get_width() + self.textSpacing * 2, self.starty))
-            if self.cursor == -1:
-                pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx, self.starty, self.opt1.get_width(), self.opt1.get_height()),  2)
-            if self.cursor == 0:
-                pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx + self.opt1.get_width() + self.textSpacing, self.starty, self.opt2.get_width(), self.opt2.get_height()),  2)
-            if self.cursor == 1:
-                pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx + self.opt1.get_width() + self.opt2.get_width() + self.textSpacing*2, self.starty, self.opt3.get_width(), self.opt3.get_height()),  2)
-
+            self.render_menu()
         pygame.display.update()
         self.fps.tick(60)
     def on_cleanup(self):
@@ -78,6 +72,20 @@ class Pong:
             self.on_loop()
         self.on_cleanup()
         
+
+    def render_menu(self):
+        self.screen.fill(BLACK)
+        self.ballObj.draw(self.screen, (self.screen.get_width(),self.screen.get_height()))
+        self.screen.blit(self.opt1, (self.startx, self.starty))
+        self.screen.blit(self.opt2, (self.startx + self.opt1.get_width() + self.textSpacing, self.starty))
+        self.screen.blit(self.opt3, (self.startx + self.opt1.get_width() + self.opt2.get_width() + self.textSpacing * 2, self.starty))
+        if self.cursor == -1:
+            pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx, self.starty, self.opt1.get_width(), self.opt1.get_height()),  2)
+        if self.cursor == 0:
+            pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx + self.opt1.get_width() + self.textSpacing, self.starty, self.opt2.get_width(), self.opt2.get_height()),  2)
+        if self.cursor == 1:
+            pygame.draw.rect(self.screen, RED, pygame.Rect(self.startx + self.opt1.get_width() + self.opt2.get_width() + self.textSpacing*2, self.starty, self.opt3.get_width(), self.opt3.get_height()),  2)
+
 if __name__ == "__main__" :
     playPong = Pong()
     playPong.on_execute()
