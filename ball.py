9034ea6efaf7d3d18c9cc3d146cd93421f4915af
NotSwan <31709744+NotSwan@users.noreply.github.com>
2021-12-02 12:07:08 -0500

    Basic Ball draw w/ movement and img

--- ball.py
+++ ball.py
@@ -4,18 +4,16 @@ import pygame.surface
 class ball(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        pygame.Surface([1,1])
-        self.pos = 0
-        self.speed = 1
-    
-    def __init__(self, size, speed):
-        super().__init__()
-        pygame.surface([size])
-        self.speed = speed
-        self.pos = 0
+        self.ballImg = pygame.image.load('ball.png')
+        self.ballSurf = pygame.transform.scale(self.ballImg, (30,30))
+        self.pos = (150,300)
+        self.speed = 5
 
-    def draw(self):
-        pass
-
+    def draw(self, screen, size):
+        screen.blit(self.ballSurf, self.pos)
+        self.pos = (self.pos[0] + self.speed, self.pos[1])
+        print(f'{self.pos[0]=} {size[0]=}')
+        if(int(self.pos[0]) >  int(size[0]) or int(self.pos[0]) < int(0)):
+            self.speed = self.speed * -1
     def speedUp(self, newSpeed):
         self.speed = newSpeed
